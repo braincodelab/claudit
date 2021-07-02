896d74da694d39242a5dd620e85a935669f2dd97
Matt Sharrock <34248297+msharrock@users.noreply.github.com>
2021-07-02 16:52:42 -0400

    0.0.1

---
+++ claudit/platforms/aws/aws_deid.py
@@ -0,0 +1,6 @@
+# https://github.com/aws-samples/amazon-comprehend-medical-image-deidentification
+# https://github.com/microsoft/InnerEye-Gateway
+
+## actually https://docs.microsoft.com/en-us/azure/healthcare-apis/fhir-paas-portal-quickstart.
+# https://techcommunity.microsoft.com/t5/healthcare-and-life-sciences/introducing-the-medical-imaging-server-for-dicom/ba-p/1694397
+# https://github.com/microsoft/dicom-server
